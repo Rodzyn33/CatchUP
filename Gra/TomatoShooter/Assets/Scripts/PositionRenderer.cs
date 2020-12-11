cb5c5959dcf2abff229a50936c43d6899ec4f4dd
Rodzyn33 <75450303+Rodzyn33@users.noreply.github.com>
2020-12-12 00:25:06 +0100

    2.0

---
+++ Gra/TomatoShooter/Assets/Scripts/PositionRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRenderer : MonoBehaviour
+{
+    // Start is called before the first frame update   
+    private int sortingOrderBase=5000;
+    private Renderer myRenderer;
+    public bool staticPostion;
+    private void Awake()
+    {
+        
+        myRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        myRenderer.sortingOrder =(int)(sortingOrderBase - transform.position.y);
+        if (staticPostion == true)
+        {
+            Destroy(this);
+        }
+    }
+
+}
