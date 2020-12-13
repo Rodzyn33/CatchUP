cc9e25beddbfe744b2f9ab1f5203346b667287ed
Rodzyn33 <75450303+Rodzyn33@users.noreply.github.com>
2020-12-14 00:32:23 +0100

    2.1

---
+++ Gra/TomatoShooter/Assets/Scripts/Doors/doorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour
+{
+    public bool doorOpen;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorOpen = false;
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartCoroutine(zmianaDrzwi());
+        }
+    }
+    IEnumerator zmianaDrzwi()
+    {
+        doorOpen = !doorOpen; anim.SetBool("Open", doorOpen);
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<Collider2D>().enabled = !gameObject.GetComponent<Collider2D>().enabled;
+    }
+}
