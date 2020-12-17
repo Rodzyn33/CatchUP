1981e1ffba83e5897223287c7102677e0628adbb
Rodzyn33 <75450303+Rodzyn33@users.noreply.github.com>
2020-12-17 11:51:47 +0100

    2.4.1

---
+++ Gra/TomatoShooter/Assets/Scripts/wavetextScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wavetextScript : MonoBehaviour
+{
+    private Text score;
+    private int fala;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text>();
+        fala = GameObject.Find("GameManager").GetComponent<GameManagerScript>().wave;
+    }
+    private void LateUpdate()
+    {
+        fala = GameObject.Find("GameManager").GetComponent<GameManagerScript>().wave;
+        score.text = "Wave: " + fala;
+    }
+
+
+
+
+}
+
